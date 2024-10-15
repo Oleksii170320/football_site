3bf42cb9b643c128def04a00bc534b8223e7f8a4
Oleksii <celect1989@gmail.com>
2024-10-15 12:51:38 +0300

    TIK: Test push: Add func "Plus"

--- src/controllers/standings.py
+++ src/controllers/standings.py
@@ -3,19 +3,5 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 router = APIRouter()
 
 
-# @router.get("/{season_id}/{season_slug}", response_model=List[schemas.MatchSchemas])
-# def read_standing(request: Request, season_id: int, db: Session = Depends(get_db)):
-#
-#     regions_list = get_regions_list(db)
-#     # seasons_region = get_seasons_region(db, region_slug=region_slug)
-#     standings = get_calculate_standings(db, season_id=season_id)
-#
-#     return templates.TemplateResponse(
-#         "standings.html",
-#         {
-#             "request": request,
-#             "regions_list": regions_list,  # Список регіонів (бокове меню)
-#             # "seasons": seasons_region,  # Список цьогорічних турнірів
-#             "standings": standings,
-#         },
-#     )
+def plus(a, b):
+    return a + b
