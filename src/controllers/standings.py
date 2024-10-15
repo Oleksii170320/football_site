41798d4fe02ad1083c88cb748fead6fb85089128
Oleksii <celect1989@gmail.com>
2024-10-15 13:16:38 +0300

    TIK: Test push: Add func "Test"

--- src/controllers/standings.py
+++ src/controllers/standings.py
@@ -2,10 +2,4 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 
 router = APIRouter()
 
-
-def plus(a, b):
-    return a + b
-
-
-def test_func():
-    return "Hello!!!"
+# test/test
